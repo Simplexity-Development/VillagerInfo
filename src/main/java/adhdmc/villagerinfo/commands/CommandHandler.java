c6ef58b4a9ed8051ea71ada357b289636d52f969
IllogicalSong <45906780+illogicalsong@users.noreply.github.com>
2021-10-20 15:02:55 -0700

    Tabcomplete added, ty Peashooter101

--- src/main/java/adhdmc/villagerinfo/commands/CommandHandler.java
+++ src/main/java/adhdmc/villagerinfo/commands/CommandHandler.java
@@ -7,15 +7,31 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
-public class CommandHandler implements CommandExecutor {
+public class CommandHandler implements CommandExecutor, TabExecutor {
+
+    //TY Peashooter101
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        ArrayList<String> subCommands1 = new ArrayList<String>(Arrays.asList("help", "toggle", "reload"));
+        if (args.length == 1) {
+            return subCommands1;
+        }
+        return null;
+    }
+
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        Player user = null;
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
+        Player user;
         //checking if player, error if not
         if (sender instanceof Player) {
             user = (Player) sender;
@@ -24,16 +40,15 @@ public class CommandHandler implements CommandExecutor {
                 VillagerInfo.plugin.reloadConfig();
                 MessageHandler.loadConfigMsgs();
                 sender.sendMessage(MessageHandler.configReload);
-                return true;
             } else {
             sender.sendMessage(ChatColor.RED + "You must be a player to run this command");
-            return true;
             }
+            return true;
         }
 
         //checking for arguments
         if (args.length == 0) {
-            user.sendMessage(MessageHandler.prefix + ChatColor.translateAlternateColorCodes('&', "\n&aAuthor: &6Illogicalsong\n&aVersion:&7 ALPHA\n&aSpecial Thanks to Peashooter101"));
+            user.sendMessage(MessageHandler.prefix + ChatColor.translateAlternateColorCodes('&', "\n&aAuthor: &6IllogicalSong\n&aVersion:&7 ALPHA\n&aSpecial Thanks to Peashooter101"));
             return true;
         }
 
@@ -76,10 +91,7 @@ public class CommandHandler implements CommandExecutor {
             return true;
         }
         //why ppl tryina put too many words tho
-        if(args.length > 1){
-            user.sendMessage(MessageHandler.prefix + " " + MessageHandler.noCommand);
-            return true;
-        }
+        user.sendMessage(MessageHandler.prefix + " " + MessageHandler.noCommand);
         return true;
     }
     //Toggle
