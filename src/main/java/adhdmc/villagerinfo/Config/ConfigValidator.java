f67e56dc49da3e12eb09ccec54d23bf7fb8139e0
IllogicalSong <45906780+illogicalsong@users.noreply.github.com>
2022-07-29 22:54:55 -0700

    more config work

--- src/main/java/adhdmc/villagerinfo/Config/ConfigValidator.java
+++ src/main/java/adhdmc/villagerinfo/Config/ConfigValidator.java
@@ -5,26 +5,58 @@ import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.configuration.file.FileConfiguration;
 
-public class ConfigValidator {
+import java.util.Locale;
 
-    public static String soundErrorMsg(String s){
+public class ConfigValidator {
+     public static String prefixString, toggleOnString,
+     toggleOffString, noPermissionString, noSubCommandString,
+     configReloadedString, helpMainString, helpToggleString,
+     helpReloadString, notAPlayerString, villagerProfessionString,
+     villagerJobsiteString, villagerLastWorkedString,
+     villagerRestocksString, villagerHomeString, villagerSleptString,
+     villagerInventoryString, reputationString,
+     noneString, neverString, emptyString;
+     public static Sound configSound = null;
+
+    public static void configValidator(){
+        prefixString = "";
+        toggleOnString = "";
+        toggleOffString = "";
+        noPermissionString = "";
+        noSubCommandString = "";
+        configReloadedString = "";
+        helpMainString = "";
+        helpToggleString = "";
+        helpReloadString = "";
+        notAPlayerString = "";
+        villagerProfessionString = "";
+        villagerJobsiteString = "";
+        villagerLastWorkedString = "";
+        villagerRestocksString = "";
+        villagerHomeString = "";
+        villagerSleptString = "";
+        villagerInventoryString = "";
+        reputationString = "";
+        noneString = "";
+        neverString = "";
+        emptyString = "";
+        configSound = null;
         FileConfiguration config = VillagerInfo.plugin.getConfig();
-        Sound configSound = null;
+        FileConfiguration locale = VillagerInfo.localeConfig.getlocaleConfig();
         try {
-            configSound = Sound.valueOf(config.getString("Sound"));
-        } catch (IllegalArgumentException e) {
-            s = ChatColor.RED + "Configuration Error: Invalid Sound. Please choose from these options.\nhttps://hub.spigotmc.org/javadocs/bukkit/org/bukkit/Sound.html";
+            configSound = Sound.valueOf(config.getString("Sound").toUpperCase(Locale.ROOT));
+        } catch (IllegalArgumentException | NullPointerException e) {
+            VillagerInfo.plugin.getLogger().warning("Configuration Error: " + configSound + " is not a valid sound! Please supply a valid sound");
         }
-        if (configSound != null) {
-            s=null;
+        if(config.getInt("highlight-time") <= 0){
+            VillagerInfo.plugin.getLogger().warning("Configuration Error: Invalid highlight time. If you would like to disable this feature, please set 'highlight-workstation' to 'false'. Otherwise please use an integer greater than zero");
         }
-        return s;
     }
 
-    public static String timeErrorMsg(String s){
+     timeErrorMsg(String s){
         FileConfiguration config = VillagerInfo.plugin.getConfig();
         int configTime = config.getInt("Length of time to highlight workstation");
-        if (!(configTime > 0)){
+        if (configTime <= 0){
             s = ChatColor.RED + "Configuration Error: Invalid highlight time.\nIf you would like to disable this feature,\nplease set 'highlight workstation' to false.\nOtherwise please use an integer greater than zero";
         }
         return s;
